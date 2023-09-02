6f37497687ed00c78d1d4221d0e700bc314f771c
ReneeOscata <ingenieria4.transmeta@gmail.com>
2023-09-02 16:30:34 -0500

    prueba vercel y postgres, python, name project, sh

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@
 set -e
 
 # Upgrade pip
-python3.11 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # Build the project
 echo "Building the project..."
-python3.11 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
