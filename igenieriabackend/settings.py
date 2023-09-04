4d75f10be2328040b925d42b97db8f2d5dfc5955
Liudmila Ilicheva <lilicheva@TRANSMETA.MRC>
2023-09-04 13:26:03 -0500

    Prueba csrf_exempt 3

--- igenieriabackend/settings.py
+++ igenieriabackend/settings.py
@@ -56,12 +56,16 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware'
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://mi-aplicacion.com',
-    'https://otra-aplicacion.com',
-    'http://192.168.7.22:3000',
-    'http://localhost:3000',
+#CORS_ALLOWED_ORIGINS = [
+#    'http://mi-aplicacion.com',
+#    'https://otra-aplicacion.com',
+#    'http://192.168.7.22:3000',
+#    'http://localhost:3000',
     # Agrega aquí los orígenes permitidos
+#]
+
+CORS_ALLOWED_ORIGINS = [
+    '*'
 ]
 
 ROOT_URLCONF = 'igenieriabackend.urls'
