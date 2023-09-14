6e7787d959e843f3fb4b3a03c56654c829b49f80
ReneeOscata <ingenieria4.transmeta@gmail.com>
2023-09-13 21:33:41 -0500

    Prueba login post

--- queryapp/views.py
+++ queryapp/views.py
@@ -11,15 +11,31 @@ from .utils.fem import fem
 
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
+from rest_framework.response import Response
 
-@api_view(['GET'])
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+
+@api_view(['POST'])
 def login(request):
-    user = User.objects.get(username='admin')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response("Usuario inválido")
+    
+    pwd_valid = check_password(password,user.password)
+
+    if not pwd_valid:
+        return Response("Contraseña inválida")
 
     token = Token.objects.create(user=user)
 
     print(token.key)
+    return Response(token.key)
 
 
 # Create your views here.
